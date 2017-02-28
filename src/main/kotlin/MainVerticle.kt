808c55203e97a88c6afaced0610df63cca1bc89c
Francesco Vasco <fsco_v-github@yahoo.it>
2017-02-28 22:22:00 +0100

    simple pure vert.x coroutine

--- src/main/kotlin/MainVerticle.kt
+++ src/main/kotlin/MainVerticle.kt
@@ -1,19 +1,16 @@
-import coroutine.asyncTimer
+import coroutine.asyncFuture
+import coroutine.handle
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 
 class MainVerticle : AbstractVerticle() {
 
     override fun start(future: Future<Void>) {
-        vertx.setTimer(1000) { event ->
-            println("timer result $event")
+        asyncFuture {
+            println("Do job...")
+            val result = handle <Long> { handler -> vertx.setTimer(2000, handler) }
+            println("result is $result")
+            future.complete()
         }
-
-        coroutine.future {
-            val result = vertx.asyncTimer(1000)
-            println("coroutine result $result")
-        }
-
-        future.complete()
     }
 }
--- src/main/kotlin/coroutine/Vertx.kt
+++ src/main/kotlin/coroutine/Vertx.kt
@@ -1,12 +1,42 @@
 package coroutine
 
-import io.vertx.core.Vertx
-import kotlin.coroutines.experimental.Continuation
-import kotlin.coroutines.experimental.suspendCoroutine
+import io.vertx.core.AsyncResult
+import io.vertx.core.Future
+import io.vertx.core.Handler
+import kotlin.coroutines.experimental.*
 
-suspend fun Vertx.asyncTimer(delay: Long): Long =
-        suspendCoroutine<Long> { cont: Continuation<Long> ->
-            setTimer(delay) { event ->
-                cont.resume(event)
+/**
+ * Create a coroutine
+ */
+fun <T> asyncFuture(context: CoroutineContext = EmptyCoroutineContext, block: suspend () -> T): Future<T> =
+        VertxFutureCoroutine<T>(context).also { block.startCoroutine(completion = it) }
+
+class VertxFutureCoroutine<T>(override val context: CoroutineContext, private val delegate: Future<T> = Future.future()) :
+        Future<T> by delegate, Continuation<T> {
+
+    override fun resume(value: T) = delegate.complete(value)
+
+    override fun resumeWithException(exception: Throwable) = delegate.fail(exception)
+}
+
+/**
+ * Await for resume and return result
+ */
+suspend fun <T> handle(block: (handler: Handler<T>) -> T): T =
+        suspendCoroutine <T> { cont: Continuation<T> ->
+            // handler calls `resume`
+            val handler = Handler<T> { cont.resume(it) }
+            block(handler)
+        }
+
+/**
+ * Await for [AsyncResult] and return result
+ */
+suspend fun <T> handleResult(block: (handler: Handler<AsyncResult<T>>) -> T): T =
+        suspendCoroutine <T> { cont: Continuation<T> ->
+            // handler returns result or exception
+            val handler = Handler<AsyncResult<T>> {
+                if (it.succeeded()) cont.resume(it.result()) else cont.resumeWithException(it.cause())
             }
+            block(handler)
         }

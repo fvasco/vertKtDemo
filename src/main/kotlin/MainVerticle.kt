7b827f39a002a4b31fe89dc02fa6255c03104806
Francesco Vasco <fsco_v-github@yahoo.it>
2017-03-01 21:40:22 +0100

    simple vert.x actor

--- src/main/kotlin/MainVerticle.kt
+++ src/main/kotlin/MainVerticle.kt
@@ -1,16 +1,27 @@
-import coroutine.asyncFuture
 import coroutine.handle
+import coroutine.handleResult
+import coroutine.launchConsumer
+import coroutine.launchFuture
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
+import io.vertx.core.eventbus.Message
 
 class MainVerticle : AbstractVerticle() {
 
-    override fun start(future: Future<Void>) {
-        asyncFuture {
+    override fun start(startFuture: Future<Void>) {
+        val eventBus = vertx.eventBus()
+
+        eventBus.launchConsumer<String>("messageBus") { message ->
+            println("Received ${message.body()}, reply ok")
+            message.reply("ok")
+        }
+
+        launchFuture(handler = (startFuture as Future<Unit>).completer()) {
             println("Do job...")
             val result = handle <Long> { handler -> vertx.setTimer(2000, handler) }
-            println("result is $result")
-            future.complete()
+            println("result is $result, send finish")
+            val response = handleResult <Message<String>> { eventBus.send("messageBus", "finish", it) }
+            println("reply is ${response.body()}")
         }
     }
 }
--- src/main/kotlin/coroutine/Vertx.kt
+++ src/main/kotlin/coroutine/Vertx.kt
@@ -3,15 +3,19 @@ package coroutine
 import io.vertx.core.AsyncResult
 import io.vertx.core.Future
 import io.vertx.core.Handler
+import io.vertx.core.eventbus.EventBus
+import io.vertx.core.eventbus.Message
+import io.vertx.core.eventbus.MessageConsumer
+import io.vertx.core.streams.ReadStream
 import kotlin.coroutines.experimental.*
 
-/**
- * Create a coroutine
- */
-fun <T> asyncFuture(context: CoroutineContext = EmptyCoroutineContext, block: suspend () -> T): Future<T> =
-        VertxFutureCoroutine<T>(context).also { block.startCoroutine(completion = it) }
+typealias MessageHandler<T> = Handler<Message<T>>
+typealias AsyncResultHandler<T> = Handler<AsyncResult<T>>
 
-class VertxFutureCoroutine<T>(override val context: CoroutineContext, private val delegate: Future<T> = Future.future()) :
+/**
+ * Vert.x Future adapter for Kotlin coroutine continuation
+ */
+private class VertxFutureCoroutine<T>(override val context: CoroutineContext, private val delegate: Future<T> = Future.future()) :
         Future<T> by delegate, Continuation<T> {
 
     override fun resume(value: T) = delegate.complete(value)
@@ -20,9 +24,33 @@ class VertxFutureCoroutine<T>(override val context: CoroutineContext, private va
 }
 
 /**
+ * Create a coroutine
+ */
+fun <T> launchFuture(handler: AsyncResultHandler<T>? = null,
+                     context: CoroutineContext = EmptyCoroutineContext,
+                     block: suspend () -> T): Future<T> =
+        VertxFutureCoroutine<T>(context).also { future ->
+            if (handler != null) future.setHandler(handler)
+            block.startCoroutine(completion = future)
+        }
+
+/**
+ * Run a message consumer for a given address
+ */
+fun <T> EventBus.launchConsumer(address: String, block: (Message<T>) -> Unit): MessageConsumer<T> =
+        consumer<T>(address).apply {
+            launchFuture {
+                forEach(block)
+            }
+        }
+
+fun <T> EventBus.launchConsumer(address: String, handler: MessageHandler<T>): MessageConsumer<T> =
+        launchConsumer(address) { handler.handle(it) }
+
+/**
  * Await for resume and return result
  */
-suspend fun <T> handle(block: (handler: Handler<T>) -> T): T =
+suspend fun <T> handle(block: (handler: Handler<T>) -> Unit): T =
         suspendCoroutine <T> { cont: Continuation<T> ->
             // handler calls `resume`
             val handler = Handler<T> { cont.resume(it) }
@@ -32,11 +60,18 @@ suspend fun <T> handle(block: (handler: Handler<T>) -> T): T =
 /**
  * Await for [AsyncResult] and return result
  */
-suspend fun <T> handleResult(block: (handler: Handler<AsyncResult<T>>) -> T): T =
+suspend fun <T> handleResult(block: (handler: AsyncResultHandler<T>) -> Unit): T =
         suspendCoroutine <T> { cont: Continuation<T> ->
             // handler returns result or exception
-            val handler = Handler<AsyncResult<T>> {
+            val handler = AsyncResultHandler<T> {
                 if (it.succeeded()) cont.resume(it.result()) else cont.resumeWithException(it.cause())
             }
             block(handler)
         }
+
+suspend fun <T> ReadStream<T>.forEach(block: (T) -> Unit): Unit {
+    suspendCoroutine <Unit> { cont: Continuation<Unit> ->
+        handler { block(it) }
+        endHandler { cont.resume(Unit) }
+    }
+}
